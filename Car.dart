38a75f9ebfb27a44314879b673a22532f9afbcad
IstiakPolok <fatin15-1944@diu.edu.bd>
2024-09-27 23:41:31 +0600

    initial commit

--- Car.dart
+++ Car.dart
@@ -1,13 +1,13 @@
 class Car {
-
+  // public properties
   String brand;
   String model;
   int year;
 
-
+  // constructor
   Car(this.brand, this.model, this.year);
 
-
+  // car age calculation method
   int carAge() {
     int currentYear = DateTime.now().year;
     return currentYear - year;
--- main.dart
+++ main.dart
@@ -1,10 +1,10 @@
 import 'Car.dart';
 
 void main() {
-
+  // Create an instance of the Car
   Car myCar = Car('Toyota', 'Corolla', 2015);
 
-
+  // Display the car's brand, model, year, and calculated age
   print('Brand: ${myCar.brand}');
   print('Model: ${myCar.model}');
   print('Year: ${myCar.year}');
